54ff409f8364146f8f206b1d6f91a9c17f6ac35d
B Brucato <bebrucato@gmail.com>
2021-03-16 15:21:17 -0700

    added the port/app.use function

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require('express');
 const path = require('path');
 const dbNotes = require('./db/db.json');
 
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(express.json());
+
