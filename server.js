dc4b05c1598bc30cb8f34ddd0278de3384c73ef2
B Brucato <bebrucato@gmail.com>
2021-03-16 15:55:04 -0700

    figuring out uuid/need more work

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const path = require('path');
 const dbNotes = require('./db/db.json');
 
+
 const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(express.json());
+
+app.get()
 
