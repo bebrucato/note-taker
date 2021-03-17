fe33703a1999dcd97afb14c86454db837e19c96f
B Brucato <bebrucato@gmail.com>
2021-03-16 19:17:41 -0700

    changed urls

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.json());
 
 //Routes retrieving the HTML files
 app.get("/notes", function (req,res) {
-    res.sendFile(path.join(__dirname, "Develop\public\notes.html"))
+    res.sendFile(path.join(__dirname, "\public\notes.html"))
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "Develop\public\index.html"))
+    res.sendFile(path.join(__dirname, "\public\index.html"))
 });
 
 //Route retrieving the API for the saved notes and joins them with the "db.json"
