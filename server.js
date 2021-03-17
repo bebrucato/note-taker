042cd731845a9865e2baccce2bf64f793f330c91
B Brucato <bebrucato@gmail.com>
2021-03-16 20:21:16 -0700

    deleted the develop folder/re-house files and fixed css issue

--- server.js
+++ server.js
@@ -14,16 +14,24 @@ app.use(express.json());
 
 //Routes retrieving the HTML files
 app.get("/notes", function (req,res) {
-    res.sendFile(path.join(__dirname, "Develop/public/notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "Develop/public/index.html"))
+    res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
 //Route retrieving the API for the saved notes and joins them with the "db.json"
 app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "Develop/db/db.json"))
+    res.sendFile(path.join(__dirname, "db/db.json"))
+});
+
+app.post("/api/notes", (req, res) => {
+    const dbNotes = JSON.parse(fs.readFileSync("db/db.json"));
+    const newNotes = req.body;
+    dbNotes.push(newNotes);
+    fs.writeFileSync("db/db.json", JSON.stringify(dbNotes));
+    res.json(dbNotes);
 });
 
 app.listen(PORT, function() {
