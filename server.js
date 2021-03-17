d81619aafa822c76a7da38adb219120cb0becf1b
B Brucato <bebrucato@gmail.com>
2021-03-16 18:50:48 -0700

    removed the const for the db.json/ added its api call

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const dbNotes = require('./db/db.json');
 
 
+//Express/PORT
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+//Registering the middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get()
+//Routes retrieving the HTML files
+app.get("/notes", function (req,res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+//Route retrieving the API for the saved notes and joins them with the "db.json"
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+
 
