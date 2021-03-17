f10f7d9c2d70ba4f5314714f628c6fac38afecc5
B Brucato <bebrucato@gmail.com>
2021-03-16 19:38:58 -0700

    fixed url to index/notes

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ app.use(express.json());
 
 //Routes retrieving the HTML files
 app.get("/notes", function (req,res) {
-    res.sendFile(path.join(__dirname, "notes.html"))
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"))
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"))
 });
 
 //Route retrieving the API for the saved notes and joins them with the "db.json"
 app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
+    res.sendFile(path.join(__dirname, "Develop/db/db.json"))
 });
 
 app.listen(PORT, function() {
